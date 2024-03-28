b7ad1b1d7a8a6a6c0de85b6c40f3b13e7a275b00
lourdesreyna <reynalourdes91@gmail.com>
2024-03-28 19:19:02 -0300

    nosotros

--- front-g2/src/App.jsx
+++ front-g2/src/App.jsx
@@ -1,6 +1,6 @@
-import './App.css'
-import Nosotros from './components/nosotros/Nosotros'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Nosotros from './components/nosotros/Nosotros';
 
 function App() {
 
--- front-g2/src/components/nosotros/Nosotros.jsx
+++ front-g2/src/components/nosotros/Nosotros.jsx
@@ -1,11 +1,18 @@
-
+import {Row, Col, Container, Image} from "react-bootstrap"; 
 const Nosotros = () => {
   return (
-    <div className="nosotros">
-        <h2>Quiénes Somos</h2>
-        
-
-
+    <div className ="nosotros">
+        <h2 className ="text">Quiénes Somos</h2>
+        <Container>
+          <Row>
+            <Col>
+              texto
+            </Col>
+            <Col xs={6} md={4} >
+            <Image src="src/assets/img/img-logo.jpeg" roundedCircle />
+            </Col>
+          </Row>
+        </Container>
     </div>
   )
 }
