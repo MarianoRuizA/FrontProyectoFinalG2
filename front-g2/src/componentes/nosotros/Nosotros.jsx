e9459f3b21d33bb691c8bac0f1ed30d6c6c8ea5a
lourdesreyna <reynalourdes91@gmail.com>
2024-03-29 15:43:55 -0300

    avances de la pagina de nosotros

--- front-g2/src/componentes/nosotros/Nosotros.jsx
+++ front-g2/src/componentes/nosotros/Nosotros.jsx
@@ -2,16 +2,20 @@ import {Row, Col, Container, Image} from "react-bootstrap";
 const Nosotros = () => {
   return (
    <div className="nosotros">
-    <Container fluid>
+    <Container>
         <div className="titulo">
             <h2>Quiénes Somos</h2>
         </div>
-      <Row>
-        <Col sm={6}>
-         Image
+      <Row className="px-4 my-5">
+        <Col sm={7}>
+        <Image src="src/assets/img/img-logo1.jpg"
+        fluid
+        rounded
+        />
         </Col>
-       <Col sm={6}>
-         texto
+       <Col sm={5}>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et neque amet ratione voluptatum, obcaecati autem eaque, veritatis fuga temporibus 
+        vel nesciunt itaque? Beatae fuga est voluptatibus! Fugiat dolorem illum ut.</p>
        </Col>
       </Row>
     </Container>
