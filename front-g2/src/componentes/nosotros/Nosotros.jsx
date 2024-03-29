e55e9c16826daa04ac5bad5a4b200e569abea696
lourdesreyna <reynalourdes91@gmail.com>
2024-03-29 20:00:11 -0300

    avances de la pagina nosotros

--- front-g2/src/componentes/nosotros/Nosotros.jsx
+++ front-g2/src/componentes/nosotros/Nosotros.jsx
@@ -1,9 +1,22 @@
 import './nosotros.css';
 const Nosotros = () => {
   return (
-   <div className="nosotros">
-    
-    
+   <div className="titulo">
+    <h1>Nosotros</h1>
+    <div className="contenedor">
+    <section className="nosotros">
+      <div className="imgNosotros">
+        <img src="src/assets/img/img-1.png" alt="" />
+      </div>
+      <div className="subtitulo">
+        <h2>Nuestra Historia</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio asperiores unde cupiditate enim error? Magnam, excepturi. Debitis, accusantium velit sunt cupiditate quam consequatur. 
+        Nemo non quaerat iusto ab eum numquam.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quae officia ratione deleniti rem impedit eligendi perferendis, mollitia distinctio
+        reprehenderit aspernatur error id velit ipsum vitae culpa sequi inventore minima.</p>
+      </div>
+    </section>
+    </div>
    </div>
   )
 }
