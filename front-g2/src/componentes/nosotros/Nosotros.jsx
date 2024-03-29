0a593044efbef3d19153920e548484b38c7fb7ba
lourdesreyna <reynalourdes91@gmail.com>
2024-03-29 17:21:50 -0300

    problemas para aplicar los estilos css

--- front-g2/src/componentes/nosotros/Nosotros.jsx
+++ front-g2/src/componentes/nosotros/Nosotros.jsx
@@ -1,22 +1,25 @@
 import {Row, Col, Container, Image} from "react-bootstrap"; 
-
+import './nosotros.css';
 const Nosotros = () => {
   return (
-   <div className="nosotros">
+   <div>
     <Container>
-        <div className="titulo">
-            <h2>Quiénes Somos</h2>
+        <div>
+          <h2 className="titulo">Quiénes Somos</h2>
         </div>
-      <Row className="px-4 my-5">
-        <Col sm={7}>
+       <Row className="px-4 my-5">
+        <Col sm={6}>
         <Image className="img" src="src/assets/img/img-logo1.jpg"
         fluid
         rounded
         />
         </Col>
-       <Col sm={5}>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et neque amet ratione voluptatum, obcaecati autem eaque, veritatis fuga temporibus 
-        vel nesciunt itaque? Beatae fuga est voluptatibus! Fugiat dolorem illum ut.</p>
+       <Col sm={6}>
+        <p className="texto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et neque amet ratione voluptatum, obcaecati autem eaque, veritatis fuga temporibus 
+        vel nesciunt itaque? Beatae fuga est voluptatibus! Fugiat dolorem illum ut.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus inventore, modi esse asperiores enim, nostrum quae et nam voluptatem commodi autem repellendus quas.
+        Fuga iure recusandae aliquid ducimus earum adipisci!
+        </p>
        </Col>
       </Row>
     </Container>
