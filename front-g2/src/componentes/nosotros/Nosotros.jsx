b38e7c6e03341bf6465ecdccd93e4372e4629780
lourdesreyna <reynalourdes91@gmail.com>
2024-03-30 09:16:47 -0300

    parte de la historia del restaurante terminada

--- front-g2/src/componentes/nosotros/Nosotros.jsx
+++ front-g2/src/componentes/nosotros/Nosotros.jsx
@@ -9,15 +9,19 @@ const Nosotros = () => {
         </div>
         <div className="content">
           <h3>nuestra historia</h3>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error quam enim asperiores molestias adipisci! Iure,
-             ullam facere unde officia voluptatibus nobis est quaerat expedita eos voluptatum beatae iusto earum eligendi? 
-             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere placeat harum necessitatibus voluptatum accusantium 
-             dolore blanditiis exercitationem, voluptas,
-             earum nemo tempore molestias nulla eligendi natus iste fugiat, illum nesciunt optio?</p>
+           <p>En una esquina bulliciosa del barrio de San Telmo en Buenos Aires, cinco jóvenes emprendedores decidieron abrir un restaurante con un nombre que evocaba la esencia de su país: "Tango". 
+             Cada uno de ellos había crecido inmerso en la cultura argentina del tango y la pasión por la gastronomía local.</p>
+
+            <p>Desde el momento en que abrieron las puertas, el restaurante Tango se convirtió en un éxito instantáneo.
+             Los aromas tentadores de empanadas recién horneadas y carnes jugosas a la parrilla flotaban en el aire, atrayendo a los clientes de todas partes de la ciudad y más allá. 
+             Pero lo que realmente hacía especial a Tango era la energía contagiosa que emanaba de los cinco amigos.</p> 
+
+            <p>Con el tiempo, Tango se convirtió en mucho más que un simple restaurante, se convirtió en un lugar donde la comida, la música y la camaradería se fusionaban para crear una experiencia inolvidable.
+             Y mientras los cinco amigos continuaban compartiendo su pasión con el mundo, la leyenda del restaurante Tango seguía creciendo, una noche de tango a la vez.</p>
         </div>
       </div>
-     <div className="excellence">
-      
+     <div className="img-section">
+      <img src="src/assets/img/imagen.png" />
      </div>
     </div>
    </div>
