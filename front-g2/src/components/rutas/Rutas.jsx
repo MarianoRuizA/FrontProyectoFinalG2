d893314128336e8c5454aca84269f7aceb81e680
lourdesreyna <reynalourdes91@gmail.com>
2024-03-31 19:15:28 -0300

    router

--- front-g2/src/components/rutas/Rutas.jsx
+++ front-g2/src/components/rutas/Rutas.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../../views/nosotros/home/Home";
-import About from "../about/About";
+import Home from "../../views/home/Home";
+import Nosotros from "../../views/nosotros/Nosotros";
 
 function Rutas() {
   return (
     <>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/nosotros" element={<About/>}></Route>
+      <Route path="/nosotros" element={<Nosotros/>}></Route>
     </Routes>
     </>
   )
