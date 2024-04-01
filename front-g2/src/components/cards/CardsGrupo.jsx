52c35677d31ce088334b38df1e6c98484adaadd4
lourdesreyna <reynalourdes91@gmail.com>
2024-03-31 21:27:12 -0300

    problemas con el responsive

--- front-g2/src/components/cards/CardsGrupo.jsx
+++ front-g2/src/components/cards/CardsGrupo.jsx
@@ -1,14 +1,39 @@
-import imagen from "../../assets/img/integrante1.png";
+import './cards.css';
+import imagen3 from "../../assets/img/integrante3.png";
+import imagen4 from "../../assets/img/integrante4.png";
+import imagen5 from "../../assets/img/integrante5.png";
 
 const CardsGrupo = () => {
   return (
+<>
     <div className="card">
-        <div className="card-img">
-            <img src={imagen} alt="perfil del integrante"/>
+        <h2 className="title">Equipo</h2>
+
+        <div className="box-container">
+            <div className="box">
+                <img src={imagen3} alt="perfil del integrante"/>
+                <h3>Mariano Agustin Ruiz</h3>
+                <p>Chef Principal</p>
+            </div>
         </div>
-        <p className="name">Lourdes Reyna</p>
-        <p>Diseñadora de Interiores</p>
+
+        <div className="box-container">
+            <div className="box">
+                <img src={imagen5} alt="perfil del integrante"/>
+                <h3>martina</h3>
+                <p>Chef Principal</p>
+            </div>
+        </div>
+
+        <div className="box-container">
+            <div className="box">
+                <img src={imagen4} alt="perfil del integrante"/>
+                <h3>Lourdes Reyna</h3>
+                <p>Chef Principal</p>
+            </div>
+        </div>
     </div>
+</>
 )
 }
 export default CardsGrupo
