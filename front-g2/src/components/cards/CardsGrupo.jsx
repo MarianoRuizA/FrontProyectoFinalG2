1c875a374970e0807386db9a9033e0f9c0053b1c
lourdesreyna <reynalourdes91@gmail.com>
2024-03-31 21:46:37 -0300

    tarjetas de los integrantes falta el css

--- front-g2/src/components/cards/CardsGrupo.jsx
+++ front-g2/src/components/cards/CardsGrupo.jsx
@@ -1,4 +1,6 @@
 import './cards.css';
+import imagen1 from "../../assets/img/integrante1.png";
+import imagen2 from "../../assets/img/integrante2.png";
 import imagen3 from "../../assets/img/integrante3.png";
 import imagen4 from "../../assets/img/integrante4.png";
 import imagen5 from "../../assets/img/integrante5.png";
@@ -19,6 +21,14 @@ const CardsGrupo = () => {
 
         <div className="box-container">
             <div className="box">
+                <img src={imagen2} alt="perfil del integrante"/>
+                <h3>Mariano Agustin Ruiz</h3>
+                <p>Chef Principal</p>
+            </div>
+        </div>
+
+        <div className="box-container">
+            <div className="box">
                 <img src={imagen5} alt="perfil del integrante"/>
                 <h3>martina</h3>
                 <p>Chef Principal</p>
@@ -28,6 +38,14 @@ const CardsGrupo = () => {
         <div className="box-container">
             <div className="box">
                 <img src={imagen4} alt="perfil del integrante"/>
+                <h3>Lourdes Reyna</h3>
+                <p>Chef Principal</p>
+            </div>
+        </div>
+
+        <div className="box-container">
+            <div className="box">
+                <img src={imagen1} alt="perfil del integrante"/>
                 <h3>Lourdes Reyna</h3>
                 <p>Chef Principal</p>
             </div>
