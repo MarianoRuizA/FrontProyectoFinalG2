3b8239466cae29e94de1ecd454883a68cde97976
lourdesreyna <reynalourdes91@gmail.com>
2024-04-01 23:08:10 -0300

    cards con estilos css responsive problemas con el hr no toma el color que le aplico

--- front-g2/src/components/cards/CardsGrupo.jsx
+++ front-g2/src/components/cards/CardsGrupo.jsx
@@ -1,113 +1,100 @@
 import './cards.css';
-import imagen1 from "../../assets/img/integrante1.png";
-import imagen2 from "../../assets/img/integrante2.png";
-import imagen3 from "../../assets/img/integrante3.png";
-import imagen4 from "../../assets/img/integrante4.png";
-import imagen5 from "../../assets/img/integrante5.png";
 
 const CardsGrupo = () => {
   return (
 <>
-    <div className="card">
-        <div className="head">
-            <div className="circle">
-                <div className="img-integrante">
-                    <img src={imagen1} alt="perfil del integrante"/>
+    <div className="team-section">
+        <div className="container">
+            <div className="row">
+                <div className="title">
+                    <h2>Equipo</h2>
                 </div>
             </div>
-        </div>
-        <div className="description">
-            <h3>Gaston</h3>
-            <h4>Chef Principal</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Possimus esse quod eveniet ab adipisci.</p>
-        </div>
-        <hr />
-        <div className="contact">
-            <a href="#">Contacto</a>
-        </div>
-    </div>
 
-    <div className="card">
-        <div className="head">
-            <div className="circle">
-                <div className="img-integrante">
-                    <img src={imagen2} alt="perfil del integrante"/>
+            <div className="team-card">
+                <div className="card">
+                    <div className="img-section">
+                        <img src="src/assets/img/integrante1.png" alt="perfil"/>
+                    </div>
+                    <div className="content">
+                        <h3>Gastón</h3>
+                        <h4>Chef Principal</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Doloribus voluptates ducimus laudantium nam nemo accusantium ipsa!</p>
+                    </div>
+                    <hr />
+                    <div className="contact">
+                        <a href="#">Contacto</a>
+                    </div>
+                </div>
+                
+                <div className="card">
+                    <div className="img-section">
+                        <img src="src/assets/img/integrante2.png" alt="perfil"/>
+                    </div>
+                    <div className="content">
+                        <h3>Gastón</h3>
+                        <h4>Chef Principal</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Doloribus voluptates ducimus laudantium nam nemo accusantium ipsa!</p>
+                    </div>
+                    <hr />
+                    <div className="contact">
+                        <a href="#">Contacto</a>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <div className="img-section">
+                        <img src="src/assets/img/integrante3.png" alt="perfil"/>
+                    </div>
+                    <div className="content">
+                        <h3>Gastón</h3>
+                        <h4>Chef Principal</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Doloribus voluptates ducimus laudantium nam nemo accusantium ipsa!</p>
+                    </div>
+                    <hr />
+                    <div className="contact">
+                        <a href="#">Contacto</a>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <div className="img-section">
+                        <img src="src/assets/img/integrante4.png" alt="perfil"/>
+                    </div>
+                    <div className="content">
+                        <h3>Gastón</h3>
+                        <h4>Chef Principal</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Doloribus voluptates ducimus laudantium nam nemo accusantium ipsa!</p>
+                    </div>
+                    <hr />
+                    <div className="contact">
+                        <a href="#">Contacto</a>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <div className="img-section">
+                        <img src="src/assets/img/integrante5.png" alt="perfil"/>
+                    </div>
+                    <div className="content">
+                        <h3>Gastón</h3>
+                        <h4>Chef Principal</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Doloribus voluptates ducimus laudantium nam nemo accusantium ipsa!</p>
+                    </div>
+                    <hr />
+                    <div className="contact">
+                        <a href="#">Contacto</a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className="description">
-            <h3>Gaston</h3>
-            <h4>Chef Principal</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Possimus esse quod eveniet ab adipisci.</p>
-        </div>
-        <hr />
-        <div className="contact">
-            <a href="#">Contacto</a>
+
         </div>
     </div>
-
-    <div className="card">
-        <div className="head">
-            <div className="circle">
-                <div className="img-integrante">
-                    <img src={imagen3} alt="perfil del integrante"/>
-                </div>
-            </div>
-        </div>
-        <div className="description">
-            <h3>Gaston</h3>
-            <h4>Chef Principal</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Possimus esse quod eveniet ab adipisci.</p>
-        </div>
-        <hr />
-        <div className="contact">
-            <a href="#">Contacto</a>
-        </div>
-    </div>
-
-    <div className="card">
-        <div className="head">
-            <div className="circle">
-                <div className="img-integrante">
-                    <img src={imagen4} alt="perfil del integrante"/>
-                </div>
-            </div>
-        </div>
-        <div className="description">
-            <h3>Gaston</h3>
-            <h4>Chef Principal</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Possimus esse quod eveniet ab adipisci.</p>
-        </div>
-        <hr />
-        <div className="contact">
-            <a href="#">Contacto</a>
-        </div>
-    </div>
-
-    <div className="card">
-        <div className="head">
-            <div className="circle">
-                <div className="img-integrante">
-                    <img src={imagen5} alt="perfil del integrante"/>
-                </div>
-            </div>
-        </div>
-        <div className="description">
-            <h3>Gaston</h3>
-            <h4>Chef Principal</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Possimus esse quod eveniet ab adipisci.</p>
-        </div>
-        <hr />
-        <div className="contact">
-            <a href="#">Contacto</a>
-        </div>
-    </div>
-
 </>
 )
 }
