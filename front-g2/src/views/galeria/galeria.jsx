25199c53d5f4739bedb9cb5005ea7ee4da90d705
Martina Gutierrez <martugutierrez166@gmail.com>
2024-04-02 19:00:53 -0300

    maquetado solucionado + agregado de imágenes y título

--- front-g2/src/views/galeria/galeria.jsx
+++ front-g2/src/views/galeria/galeria.jsx
@@ -1,4 +1,4 @@
-import { Row, Container, Button } from "react-bootstrap"
+
 import Imagen from "./imgGalerias"
 import "./galeria.css"
 
@@ -8,20 +8,51 @@ const Galeria = () => {
         <>
             <h1>Esto es galeria</h1>
             <main>
+
                 <section>
-                    <Container >
+                    <article className="m-3 p-3">
+                        <h3>UN LUGAR QUE HABLA POR SI MISMO…</h3>
+                        <h5>Te invitammos a conocer el diseño de una Gatronomia y un ambiente diferente.
+                            Su historia, la calidad y la diferenciacion, lo convierten en una de las citas obligadas de la gastronomia de Tucumán.
 
-                        <Row className="justify-content-center align-content-center flex-wrap">
-                            <Imagen link="https://doroitalianbar.com/wp-content/uploads/2021/08/MAE0944-1030x1030.jpg" nombre= "Primer plato" />
+                        </h5>
+                    </article>
+                    <article>
+                        <div className="divGaleria">
 
-                            <Imagen link="https://doroitalianbar.com/wp-content/uploads/2021/08/MAE5032-1030x1030.jpg" nombre= "Segundo plato" />
-                            
-                            <Imagen link="https://doroitalianbar.com/wp-content/uploads/2021/08/MAE5686-1-1030x1030.jpg" nombre="Barra de servicios"/>
 
-                            <Imagen link= "https://doroitalianbar.com/wp-content/uploads/2021/08/MAE0951-1-1030x1030.jpg" nombre= "Tercer plato" />
-                        </Row>
-                    </Container>
-                    <Button>hola</Button>
+                            <Imagen link="https://doroitalianbar.com/wp-content/uploads/2021/08/MAE0944-1030x1030.jpg" nombre="Primer plato" />
+
+
+                            <Imagen link="https://doroitalianbar.com/wp-content/uploads/2021/08/MAE5686-1-1030x1030.jpg" nombre="Barra de servicios" />
+
+
+                            <Imagen link="https://doroitalianbar.com/wp-content/uploads/2021/08/MAE5032-1030x1030.jpg" nombre="Segundo plato" />
+
+
+
+                            <Imagen link="https://doroitalianbar.com/wp-content/uploads/2021/08/MAE0951-1-1030x1030.jpg" nombre="Tercer plato" />
+
+                            <Imagen link="https://doroitalianbar.com/wp-content/uploads/2021/08/MAE5196-1030x1030.jpg" nombre="Cuarto plato" />
+
+                            <Imagen link="https://doroitalianbar.com/wp-content/uploads/2021/08/MAE5057-1030x1030.jpg" nombre="Quinto plato" />
+
+                            <Imagen link="https://doroitalianbar.com/wp-content/uploads/2021/08/MAE5113-1030x1030.jpg" nombre="Sexto plato" />
+
+                            <Imagen link="https://doroitalianbar.com/wp-content/uploads/2021/08/MAE5128-1030x1030.jpg" nombre="Séptimo plato" />
+
+                            <Imagen link="https://doroitalianbar.com/wp-content/uploads/2021/08/IMG-8146-1030x1024.jpg" nombre="Octavo plato" />
+
+                            <Imagen link="src/views/galeria/img/noveno-plato.jpg" nombre="Noveno plato" />
+
+                            <Imagen link="src/views/galeria/img/mesas-bar.jpg" nombre="Mesas" />
+
+                            <Imagen link="src/views/galeria/img/ultimo-plato.jpg" nombre="Décimo plato" />
+                        </div>
+
+
+
+                    </article>
                 </section>
 
             </main>
--- front-g2/src/views/galeria/imgGalerias.jsx
+++ front-g2/src/views/galeria/imgGalerias.jsx
@@ -1,10 +1,16 @@
-import { Col, Image } from "react-bootstrap"
 
+import './galeria.css'
 const Imagen = ({ link, nombre }) => {
     return (
 
+        <>
+        <figure className="col-12 col-sm-6 col-lg-3">
         
-        <Image src={link} alt={nombre} width={400} height={400} col-xs={12} col-sm={9} col-md={6} col-lg={6}/>
+
+        <img src={link} alt={nombre} />
+        
+        </figure>
+        </>
         
 
 
