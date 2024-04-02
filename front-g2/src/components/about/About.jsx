8cc305632e1332c2fb9c669d799f2f3e6a00d840
lourdesreyna <reynalourdes91@gmail.com>
2024-04-02 19:45:50 -0300

    about us

--- front-g2/src/components/about/About.jsx
+++ front-g2/src/components/about/About.jsx
@@ -12,14 +12,11 @@ const About = () => {
         <div className="contenido-nosotros">
           <h3>nuestra historia</h3>
            <p>En una esquina bulliciosa del barrio de San Telmo en Buenos Aires, cinco jóvenes emprendedores decidieron abrir un restaurante con un nombre que evocaba la esencia de su país: "Tango". 
-             Cada uno de ellos había crecido inmerso en la cultura argentina del tango y la pasión por la gastronomía local.</p>
-
-            <p>Desde el momento en que abrieron las puertas, el restaurante Tango se convirtió en un éxito instantáneo.
-             Los aromas tentadores de empanadas recién horneadas y carnes jugosas a la parrilla flotaban en el aire, atrayendo a los clientes de todas partes de la ciudad y más allá. 
-             Pero lo que realmente hacía especial a Tango era la energía contagiosa que emanaba de los cinco amigos.</p> 
-
+             Cada uno de ellos había crecido inmerso en la cultura argentina del tango y la pasión por la gastronomía local.</p> 
+            <p>Desde el momento en que abrieron las puertas, el restaurante Tango se convirtió en un éxito instantáneo. Los aromas tentadores de empanadas recién horneadas y carnes jugosas a la parrilla flotaban en el aire,
+              atrayendo a los clientes de todas partes de la ciudad y más allá.</p>
             <p>Con el tiempo, Tango se convirtió en mucho más que un simple restaurante, se convirtió en un lugar donde la comida, la música y la camaradería se fusionaban para crear una experiencia inolvidable.
-             Y mientras los cinco amigos continuaban compartiendo su pasión con el mundo, la leyenda del restaurante Tango seguía creciendo, una noche de tango a la vez.</p>
+              Y mientras los cinco amigos continuaban compartiendo su pasión con el mundo, la leyenda del restaurante Tango seguía creciendo, una noche de tango a la vez.</p>
         </div>
       </div>
      <div className="img-nosotros">
