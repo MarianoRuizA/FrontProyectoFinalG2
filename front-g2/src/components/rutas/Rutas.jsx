41bffbd9b6d78b4f1afa3fb3671a7ca0d2e75a07
lourdesreyna <reynalourdes91@gmail.com>
2024-04-06 16:24:03 -0300

    cambios en las carpetas

---
+++ front-g2/src/components/rutas/Rutas.jsx
@@ -0,0 +1,12 @@
+
+
+const Rutas = () => {
+  return (
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/nosotros" element={<Nosotros/>}></Route>
+  </Routes>
+  )
+}
+
+export default Rutas
