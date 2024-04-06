8de05e42fb8418381e02a8040a5b80853566a46a
lourdesreyna <reynalourdes91@gmail.com>
2024-04-06 17:01:59 -0300

    no aparece el proyecto

--- front-g2/src/components/rutas/Rutas.jsx
+++ front-g2/src/components/rutas/Rutas.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../../views/home/Home";
-import Registro from "../../views/registro/Registro"
+import FormRegistro from "../../views/registro/Registro"
 
 const Rutas = () => {
   return (
   <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/registro" element={<Registro />}></Route>
+    <Route path="/registro" element={<FormRegistro />}></Route>
   </Routes>
   )
 }
