3435f25d4ef76b564594c76c19b67d1edf87051f
lourdesreyna <reynalourdes91@gmail.com>
2024-04-06 17:25:21 -0300

    no aparece el proyecto en la pantalla

--- front-g2/src/components/rutas/Rutas.jsx
+++ front-g2/src/components/rutas/Rutas.jsx
@@ -5,7 +5,7 @@ import FormRegistro from "../../views/registro/Registro"
 const Rutas = () => {
   return (
   <Routes>
-    <Route path="/" element={<Home />} />
+    <Route path="/" element={<Home />}></Route>
     <Route path="/registro" element={<FormRegistro />}></Route>
   </Routes>
   )
