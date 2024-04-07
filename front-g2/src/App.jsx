c13fbc8c3181b4e71f145b8d950ea1d84bb42a6b
lourdesreyna <reynalourdes91@gmail.com>
2024-04-07 09:40:06 -0300

    ya se puede ver, faltaba en return en el app

--- front-g2/src/App.jsx
+++ front-g2/src/App.jsx
@@ -2,9 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Registro from "../src/views/registro/Registro";
 
 function App() {
+  return (
   <>
    <Registro />
   </>
+  )
 }
 
 export default App
