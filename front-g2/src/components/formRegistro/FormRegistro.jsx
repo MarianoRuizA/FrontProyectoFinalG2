58907492b9947c97168c6659c286c64d1172b42c
lourdesreyna <reynalourdes91@gmail.com>
2024-04-08 07:23:53 -0300

    problemas con el responsive

--- front-g2/src/components/formRegistro/FormRegistro.jsx
+++ front-g2/src/components/formRegistro/FormRegistro.jsx
@@ -2,18 +2,9 @@ import "./formRegistro.css"
 
 const FormRegistro = () => {
   return (
-    <div className="registro-container">
-      <div className="registro-info">
-        <h2 className="title">Crea una cuenta</h2>
-        <form className="inputs-container">
-          <input className="input" type="text" placeholder="Nombre" />
-          <input className="input" type="email" placeholder="Email" />
-          <input className="input" type="password" placeholder="Contraseña" />
-          <button className="btn">registrate</button>
-        </form>
-      </div>
-      <img className="img-container" src="src/assets/img/img-ilustrativo.png" alt="img" />  
-    </div>
+    <>
+    
+    </>
   )
 }
 
