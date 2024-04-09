3b73493f19c63c96e695de381459b79471e4fdea
marianoruiza <marianoruiz145@gmail.com>
2024-04-08 21:26:58 -0300

    footer finalizado

--- front-g2/src/components/footer/Footer.jsx
+++ front-g2/src/components/footer/Footer.jsx
@@ -16,20 +16,20 @@ const Footer = () => {
  
        <  div className='container_div pt-lg-4'>
        
-        <div> <span  style={{color: 'gray'}}><FontAwesomeIcon style={{height:"30px"}} icon={faClock} /></span>  <h2 className='border_button'> Horarios de atencion</h2>    </div>
-         <div> <h3 className='mt-4 '>Lunes a viernes </h3> 09:00 a 24:00</div>
+        <div> <span  style={{color: 'darkred'}}><FontAwesomeIcon style={{height:"30px"}} icon={faClock} /></span>  <h2 className='border_button'> Horarios de atencion</h2>    </div>
+         <div> <h3 className='mt-lg-4 '>Lunes a viernes </h3> 09:00 a 24:00</div>
           <div><h3>Sabado </h3>12:00 a 24:00 hs</div>
           <div><h3>Domingo</h3> 12:00 a 17:00 hs </div>
       
          </div>
          <div className='container_div'>
-            <div  ><span   style={{color: 'gray'}}><FontAwesomeIcon style={{height:"30px"}} icon={faLocationDot} /></span><h2 className='border_button' >Sucursales </h2> </div>
+            <div  ><span   style={{color: 'darkred'}}><FontAwesomeIcon style={{height:"30px"}} icon={faLocationDot} /></span><h2 className='border_button' >Sucursales </h2> </div>
             <div><h3>Chacabuco 474</h3></div>
            <div><h3>San Martin 821</h3></div>
           <div><h3>24 de septiembre y congreso</h3></div>
        </div>
        <div className='container_div  '>
-          <div  ><span   style={{color: 'gray'}}><FontAwesomeIcon style={{height:"30px"}} icon={faMessage} /></span><h2 className='border_button'>Contactate</h2></div>
+          <div  ><span   style={{color: 'darkred'}}><FontAwesomeIcon style={{height:"30px"}} icon={faMessage} /></span><h2 className='border_button'>Contactate</h2></div>
           <div><a href="" className='container_div_aLink'> <h3><FontAwesomeIcon icon={faWhatsapp }    /> Whatsapp</h3></a> </div>
           <div><a href=""  className='container_div_aLink'><h3><FontAwesomeIcon icon={faInstagram} /> Instagram</h3></a> </div>
           <div><a href=""  className='container_div_aLink'><h3><FontAwesomeIcon icon={faEnvelope} /> Gmail</h3></a></div>
