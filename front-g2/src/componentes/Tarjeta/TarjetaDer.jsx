07a1f423f5c25157b85da9369b03eb4982f2db90
LucianoCV01 <Lucianocosentino01@gmail.com>
2024-04-10 22:22:51 -0300

    ultimos cambios hechos

--- front-g2/src/componentes/Tarjeta/TarjetaDer.jsx
+++ front-g2/src/componentes/Tarjeta/TarjetaDer.jsx
@@ -1,22 +1,22 @@
 import "./TarjetaDer.css"
-import Button from 'react-bootstrap/Button';
+import Boton from '../Boton/Boton'
 
 const TarjetaDer = () => {
     return (
         <>
             <section class="section-container-der row align-items-center ">
-                <div class="col-md-6 order-md-1">
+                <div class="col-md-6 order-md-1 order-2">
                     <div class="section-content p-4">
-                        <h3 class="pb-2">¡Te invitamos a disfrutar!</h3>
-                        <p class="pb-1">En TANGO, no solo te invitamos a disfrutar de deliciosos platos, sino también a compartir momentos especiales con tus seres queridos en un ambiente acogedor y familiar. ¡Ven y déjanos llevarte en un viaje culinario a Argentina!</p>
+                        <h2 class="pb-3">¡Te invitamos a disfrutar!</h2>
+                        <p class="pb-2">En TANGO, no solo te invitamos a disfrutar de deliciosos platos, sino también a compartir momentos especiales con tus seres queridos en un ambiente acogedor y familiar. ¡Ven y déjanos llevarte en un viaje culinario a Argentina!</p>
                         <div class="boton-content">
-                            <Button variant="primary" class="btn btn-md">Reservar</Button>
+                            <Boton buttonText="Reservar"></Boton>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6 order-md-2 p-0">
-                    <figure class="image-container">
-                    <img src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGFyZ2VudGlub3MlMjByZXN0YXVyYW50ZXxlbnwwfHwwfHx8MA%3D%3D" alt="" class="img-fluid w-100" />
+                <div class="col-md-6 order-md-2 order-1 p-0">
+                    <figure class="image-container m-0">
+                        <img src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGFyZ2VudGlub3MlMjByZXN0YXVyYW50ZXxlbnwwfHwwfHx8MA%3D%3D" alt="" class="img-fluid w-100" />
                     </figure>
                 </div>
             </section>
--- front-g2/src/views/paginaPrincipal/paginaPrincipal.jsx
+++ front-g2/src/views/paginaPrincipal/paginaPrincipal.jsx
@@ -8,13 +8,14 @@ const PaginaPrincipal = () => {
         <>
             <main>
                 <Banner></Banner>
+                {/* <div className="separator"></div> Elemento separador */}
                 <TarjetaDer></TarjetaDer>
                 <TarjetaIzq></TarjetaIzq>
-                <section class="contenedor-logo d-flex flex-column align-items-center justify-content-center">
-                    <figure>
+                <section class="contenedor-logo d-flex flex-column align-items-center justify-content-center pt-1 pb-1">
+                    <figure class="mb-1">
                         <img src="src/img/logo_tango.png" alt="Logo de Tango Restaurante" class="img-fluid" />
                     </figure>
-                    <h3>Tucumán - Argentina</h3>
+                    <p class="mb-1">Tucumán - Argentina</p>
                 </section>
             </main>
         </>
