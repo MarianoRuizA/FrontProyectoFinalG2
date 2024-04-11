941c121c57ae085d46264721c6bff8a92b41f850
marianoruiza <marianoruiz145@gmail.com>
2024-04-10 22:41:22 -0300

    nueva ventana realiazda

--- front-g2/src/components/reserva/Reserva.jsx
+++ front-g2/src/components/reserva/Reserva.jsx
@@ -3,13 +3,15 @@ import './reserva.css'
 import { Button } from 'react-bootstrap';
  import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
  import {faArrowLeft, faArrowRight} from "@fortawesome/free-solid-svg-icons"
- 
+ import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
  
 const reserva = () => {
 const [showSucursales, setShowSucursales]  = useState(true)
 const [showComensales, setShowComensales] = useState(false)
  const [showServicio, setShowServicio] = useState(false)
  const [showFecha, setShowFecha] = useState (false)
+ const [selectedDate, setSelectedDate] = useState(new Date());
  const [cantidad, setCantidad] = useState(1)
  const [reserva, setReserva] = useState({
   sucursal: "",
@@ -19,9 +21,10 @@ const [showComensales, setShowComensales] = useState(false)
  })
  const backComensal = () =>{ //Funciom volver para el boton
     if (showComensales){
-      setReserva({
-        sucursal: ""
-      })
+      setReserva(prevReserva => ({  // ...prevReserva conserva el estado de mi componente reserva con sus valores anteriores
+        ...prevReserva,
+          sucursal: ""
+      }));
       setShowComensales(false)
       setShowSucursales(true)
     }
@@ -33,16 +36,36 @@ const [showComensales, setShowComensales] = useState(false)
       setShowServicio(false)
       setShowComensales(true)
     }
+
+ if(showFecha){
+    setReserva(prevReserva => ({  // ...prevReserva conserva el estado de mi componente reserva con sus valores anteriores
+    ...prevReserva,
+      servicio: ""
+    }));
+
+    setShowFecha(false)
+    setShowServicio(true)
+    
  }
+
+}
 const nextComensalesFunction = ( ) => {
   setReserva(prevReserva => ({  // ...prevReserva conserva el estado de mi componente reserva con sus valores anteriores
     ...prevReserva,
     comensales: cantidad
   }));
   setShowComensales(false)
- setShowServicio(true)
+  setShowServicio(true)
 }
-
+const nextServicioFunction = (servicioElejido) =>{
+  setReserva(prevReserva => ({  // ...prevReserva conserva el estado de mi componente reserva con sus valores anteriores
+    ...prevReserva,
+    servicio: servicioElejido
+  }));
+  setShowServicio(false)
+  setShowFecha(true)
+  
+}
  return (
    
 <>
@@ -50,11 +73,11 @@ const nextComensalesFunction = ( ) => {
  <h1 style={{textAlign: "center"}}>Te damos la bienvenida !!</h1> 
     <div className='montserrat-font div_conteiner'>
       <header><Button onClick={() => backComensal()} variant='dark' className='m-3'><FontAwesomeIcon icon={faArrowLeft} /></Button>
-   <span>
-    {reserva.sucursal ? <Button variant='light'>Sucursal: {reserva.sucursal}</Button>  : null}
-     {reserva.comensales  ?  <><span>  <Button variant='light'>Comensales: {reserva.comensales}</Button></span></> : null}
-    {/* <Button variant='light'>Servicio: {reserva.servicio}</Button>
-    <Button variant='light'>Fecha: {reserva.fecha}</Button> */} 
+   <span >
+    {reserva.sucursal ? <Button   variant='light'>Sucursal: {reserva.sucursal}</Button>  : null}
+     {reserva.comensales  ? <><Button variant='light'>Comensales: {reserva.comensales}</Button></> : null}
+     {reserva.servicio ? <Button variant='light'>Servicio: {reserva.servicio}</Button>: null}
+      {reserva.fecha ? <Button variant='light'>Fecha: {reserva.fecha}</Button> : null}
    </span>
     </header>
     
@@ -80,16 +103,21 @@ const nextComensalesFunction = ( ) => {
         </>            
          : null }
       {showServicio ? <><h2>Elija el servicio que le gustaria recibir</h2>
-            <Button  className='mb-3' style={{width: "50%", height: "50px"} } variant='light'>Almuerzo</Button>
-            <Button style={{width: "50%", height: "50px"} } variant='light'>Cena</Button></> : null}
+            <Button onClick={(e)=>nextServicioFunction( e.target.textContent)} className='mb-3' style={{width: "50%", height: "50px"} } variant='light'>Almuerzo</Button>
+            <Button onClick={(e)=>nextServicioFunction( e.target.textContent)} style={{width: "50%", height: "50px"} } variant='light'>Cena</Button></> : null}
+       {showFecha ?  <> <h2>Selecciona una fecha:</h2>
+      <DatePicker selected={selectedDate} 
+  
+      onChange={date => setSelectedDate(date)} 
+      minDate={new Date()} // Establece la fecha mínima al día actual
+      maxDate={ new Date().setMonth(new Date().getMonth() + 2)} // Establece la fecha máxima a dos meses después del día actual
+      showTimeSelect
+        dateFormat={"dd/MM/yyyy HH:mm"} />
+      {selectedDate && <p>Fecha seleccionada: {selectedDate.toLocaleDateString()} </p>}
+      </> :null}      
     </div> 
   
-{/*  objeto reserva: {
-  cantidad de comensales: 0,
-  fecha: ""
-  servicio: ""
-
-}  */}
+ 
      </div>
 
 </>
