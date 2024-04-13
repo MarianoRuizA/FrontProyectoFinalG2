372d455fd44438dad52efd712f9f3bbab4e4b291
marianoruiza <marianoruiz145@gmail.com>
2024-04-13 17:05:23 -0300

    footer, cambio de colores

--- front-g2/src/App.jsx
+++ front-g2/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Footer from './components/footer/Footer'
+import Footer from './components/footer/Footer.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
