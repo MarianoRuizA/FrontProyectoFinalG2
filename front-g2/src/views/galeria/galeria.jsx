3cd712a53de9b366bd87ab71c9bed27fb35ef1db
Martina Gutierrez <martugutierrez166@gmail.com>
2024-04-15 02:27:00 -0300

    Se corrigió error de hover de imágenes

--- front-g2/src/views/galeria/galeria.jsx
+++ front-g2/src/views/galeria/galeria.jsx
@@ -9,21 +9,23 @@ import Titulo from '../../componentes/imgCarousel/titulo/titulo'
 
 const Galeria = () => {
 
-    const {imagenes} = useContext(GaleriaProvider)
+    const { imagenes } = useContext(GaleriaProvider)
     let ultInd = (imagenes.length) - 1
 
     const [imagen, setImagen] = useState({ link: '', nombre: '', i: -1 })
-    const [scroll, setScroll] = useState(0)
+    const [carouselAbierto, setCarouselAbierto] = useState(false)
 
     const handleClick = (link, nombre, i) => {
-        console.log("Current Scroll:", scroll);
         setImagen({ link, nombre, i })
+        setCarouselAbierto(true)
     }
 
     const handleOpciones = (opcion, i) => {
         let estructura;
         if (opcion === "cerrar") {
-            estructura = { link: '', nombre: '', i: -1 };
+            estructura = { link: '', nombre: '', i: -1 }
+            setCarouselAbierto(false)
+
         } else {
             const nuevoIndex = opcion === "ant" ? (i === 0 ? ultInd : i - 1) : (i === ultInd ? 0 : i + 1);
             estructura = { link: imagenes[nuevoIndex].link, nombre: imagenes[nuevoIndex].nombre, i: nuevoIndex };
@@ -31,51 +33,34 @@ const Galeria = () => {
         setImagen(estructura)
     }
 
-    useEffect(()=>
-    {
-        addEventListener("scroll", ()=>
-        {
-            setScroll(window.scrollY)
-        })
-    },[])
-   
-    useEffect(() => {
-        const fondo = document.getElementById("sectionGaleria")
-        if (imagen.i !== -1) {
-            fondo.style.display = "none"
-        }else{
-            fondo.style.display = "block"
-            window.scrollTo(0, scroll); 
-        }
-    }, [imagen])
 
     return (
+
         <>
-            {
-                imagen.link &&
-                <div id="divImagenCarousel">
-                    
-                    <img src={imagen.link} alt={imagen.nombre} />
-
-                    <button id="btnCerrar" onClick={() => handleOpciones("cerrar", imagen.i)}>
-                        <FontAwesomeIcon icon={faCircleXmark} className='iconos' />
-                    </button>
-
-                    <button id="btnAnterior" onClick={() => handleOpciones("ant", imagen.i)}
-                    >
-                        <FontAwesomeIcon icon={faCircleLeft} className='iconos' />
-                    </button>
-
-                    <button id="btnSiguiente" onClick={() => handleOpciones("sig", imagen.i)}>
-                        <FontAwesomeIcon icon={faCircleRight} className='iconos' />
-                    </button>
-                  
-                </div>
-            }
             <main>
+                {
+                    imagen.link &&
+                    <div id="divImagenCarousel">
+
+                        <img src={imagen.link} alt={imagen.nombre} />
+
+                        <button id="btnCerrar" onClick={() => handleOpciones("cerrar", imagen.i)}>
+                            <FontAwesomeIcon icon={faCircleXmark} className='iconos' />
+                        </button>
+
+                        <button id="btnAnterior" onClick={() => handleOpciones("ant", imagen.i)}
+                        >
+                            <FontAwesomeIcon icon={faCircleLeft} className='iconos' />
+                        </button>
+
+                        <button id="btnSiguiente" onClick={() => handleOpciones("sig", imagen.i)}>
+                            <FontAwesomeIcon icon={faCircleRight} className='iconos' />
+                        </button>
+                    </div>
+                }
 
                 <section id='sectionGaleria'>
-                    <Titulo link="https://doroitalianbar.com/wp-content/uploads/2021/08/179-1.jpg" texto="imagen de mesas" nombre="Galería"/>
+                    <Titulo link="https://doroitalianbar.com/wp-content/uploads/2021/08/179-1.jpg" texto="imagen de mesas" nombre="Galería" />
                     <article className="p-3 m-5">
                         <h3>UN LUGAR QUE HABLA POR SI MISMO…</h3>
                         <h5>Te invitammos a conocer el diseño de una Gatronomia y un ambiente diferente.
@@ -94,7 +79,7 @@ const Galeria = () => {
                                     <img src={foto.link}
                                         alt={foto.nombre}
                                         key={index}
-                                        onClick={() => handleClick(foto.link, foto.nombre, index)} />
+                                        onClick={() => handleClick(foto.link, foto.nombre, index)} className={carouselAbierto ? "carouselAbierto" : ""}/>
                                 </figure>
                             ))}
 
