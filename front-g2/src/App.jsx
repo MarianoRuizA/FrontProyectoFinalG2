5ec661c62d23e39fef0ff32491bc69213baa1d2f
LucianoCV01 <Lucianocosentino01@gmail.com>
2024-04-22 00:56:18 -0300

    fix merge con administracion

--- front-g2/src/App.jsx
+++ front-g2/src/App.jsx
@@ -3,14 +3,14 @@ import GaleriaContext from './context/galeriaContext';
 import UsuariosContext from "./context/UsuariosContext";
 import Navbar from "./componentes/navbar/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PaginaPrincipal from "./views/paginaPrincipal/paginaPrincipal"
+import Nosotros from './views/nosotros/Nosotros.jsx';
+import Footer from './components/footer/Footer.jsx'
 import ContextUsuarios from './context/ContextUsuarios';
 import Administracion from './views/administracion/administracion'
 import ContextReservas from './context/ContextReservas';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import PaginaPrincipal from "./views/paginaPrincipal/paginaPrincipal"
-import Nosotros from './views/nosotros/Nosotros.jsx';
-import Footer from './components/footer/Footer.jsx'
 
 function App() {
 
@@ -26,7 +26,6 @@ function App() {
          <Footer/>
          <Nosotros />
       </UsuariosContext>
-
       <ContextUsuarios>
         <ContextReservas>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -35,10 +34,9 @@ function App() {
           </LocalizationProvider>
         </ContextReservas>
       </ContextUsuarios>
-
     </>
-
   );
 }
 
-export default App
+export default App;
+
