0f6f3a9e9e35c28f8b710dbf448e3c98bfd900a9
LucianoCV01 <Lucianocosentino01@gmail.com>
2024-04-22 01:07:24 -0300

    ultimo fix del merge con administracion

--- front-g2/src/context/usuariosContext.jsx
+++ front-g2/src/context/usuariosContext.jsx
@@ -1,36 +1,53 @@
 import { createContext, useEffect, useState } from "react";
-import axios from "axios";
+import axios from "axios"
 
-export const UsuariosProvider = createContext();
 
-const UsuariosContext = ({ children }) => {
+export const UsuarioProvider = createContext()
 
-  const [usuarios, setUsuarios] = useState([])
-
-  const getUsers = async () => {
-    try {
-      const response = await axios.get("http://localhost:8000/usuarios");
-      setUsuarios(response.data);
-    } catch (error) {
-      console.log(error);
+const UsuarioContext = ({children}) =>
+{
+    const [usuarios, setUsuarios] = useState([])
+    const traerUsuarios = async () =>
+    {
+        try {
+            const response = await axios.get(`http://localhost:8000/usuarios`)
+            setUsuarios(response.data)
+        } catch (error) {
+            console.log("No funciona traerUsuarios", error)
+        }
     }
-  };
 
-  const logout = () => {
-    localStorage.removeItem("user");
-    window.location.href = "/";
-  };
+    const modificarUsuario = async (usuario) =>
+    {
+        try {
+            await axios.put(`http://localhost:8000/usuarios/${usuario.id}`, usuario)
+            traerUsuarios()
+                } catch (error) {
+            console.log("No funciona modificarUsuario-->", error)
+        }
+    }
 
-  useEffect(() => {
-    getUsers();
-  }, []);
+    const eliminarUsuario = async (usuario) =>
+    {
+        try{
+            await axios.delete(`http://localhost:8000/usuarios/${usuario.id}`)
+            traerUsuarios()
 
-  return (
-    <UsuariosProvider.Provider value={{ usuarios, getUsers, logout }}>
-      {children}
-    </UsuariosProvider.Provider>
-  )
+        }catch(error)
+        {
+            console.log("No funciona eliminarUsuario-->", error)
+        }
+    }
 
+    useEffect(()=>{
+        traerUsuarios()
+    }, [])
+
+    return(
+        <UsuarioProvider.Provider value={{usuarios, modificarUsuario, eliminarUsuario}}>
+            {children}
+        </UsuarioProvider.Provider>
+    )
 }
 
-export default UsuariosContext
+export default UsuarioContext
