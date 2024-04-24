33a4b3c4ad6bec51e13aa75392c20f8c20529970
LucianoCV01 <Lucianocosentino01@gmail.com>
2024-04-24 05:02:20 -0300

    Intento de arreglo de reservas

--- front-g2/README.md
+++ front-g2/README.md
@@ -1,8 +1,37 @@
-# React + Vite
+# Proyecto Tango
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Descripción
 
-Currently, two official plugins are available:
+"Tango" es una página web dedicada al servicio de restaurante, diseñada para satisfacer las necesidades promocionales y administrativas del equipo responsable.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Objetivo
+
+El objetivo principal de Tango es mejorar la experiencia tanto para los clientes como para el equipo interno del restaurante, destacando la calidad del servicio a través de una interfaz atractiva y fluida.
+
+## Tecnologías Utilizadas
+
+El proyecto se basa en el stack de programación MERN, que incluye:
+
+- **MongoDB:** Base de datos no relacional orientada a documentos.
+- **Express.js:** Framework de backend para Node.js.
+- **React.js:** Biblioteca para construir interfaces de usuario.
+- **Node.js:** Entorno de ejecución de JavaScript del lado del servidor.
+
+## Funcionalidades Principales
+
+- **Página Principal:** Interfaz elegante y sencilla que proporciona acceso rápido a las funciones principales del sitio.
+  
+- **Menú Principal:** Navegación eficaz que incluye las secciones de Galería, Nosotros, Carta, Reservas e Iniciar Sesión.
+
+- **Registro e Inicio de Sesión:** Permite a los clientes registrarse para acceder al servicio de reservas y proporciona un formulario de inicio de sesión para acceder a la cuenta.
+
+- **Reservas:** Los usuarios registrados pueden realizar reservas para un día y horario específico, con validación de disponibilidad y restricciones de fecha y hora.
+
+- **Administración:** Los usuarios administradores tienen acceso a una página exclusiva con herramientas de administración, incluyendo la gestión de usuarios y reservas.
+
+## Especificaciones Adicionales
+
+- Se aplican restricciones de fecha y hora según los horarios establecidos por el restaurante.
+- Se incluyen alertas de confirmación para minimizar errores al eliminar usuarios.
+
+Esta documentación proporciona una visión general del proyecto Tango y sus principales características y funcionalidades.
--- front-g2/src/components/reserva/Reserva.jsx
+++ front-g2/src/components/reserva/Reserva.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react'
 import './reserva.css'
-import { Button } from 'react-bootstrap';
+// import { Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons"
 import Fecha from '../fecha/Fecha.jsx';
@@ -12,174 +12,292 @@ import { UsuariosContext } from '../../context/ContextUsuarios.jsx';
 import Titulo from '../../componentes/imgCarousel/titulo/titulo.jsx';
 
 
-const Reserva = () => {
-  const { reservas, crearReserva } = useContext(ReservasContext)
-  const { emailLogueado } = useContext(UsuariosContext)
 
+// import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import context from 'react-bootstrap/esm/AccordionContext.js';
 
-
-  const [showSucursales, setShowSucursales] = useState(true)
-  const [showComensales, setShowComensales] = useState(false)
-  const [showServicio, setShowServicio] = useState(false)
-  const [showFecha, setShowFecha] = useState(false)
-  const [cantidad, setCantidad] = useState(1)
+const FormularioReserva = () => {
+  const { crearReserva } = useContext(ReservasContext);
   const [reserva, setReserva] = useState({
-    _id: "",
-    usuario: "",
-    sucursal: "",
-    servicio: "",
-    comensales: 0,
-    fecha: ""
+    usuario: '',
+    sucursal: '',
+    servicio: '',
+    comensales: '',
+    fecha: ''
+  });
 
-  })
-
-
-  const actualizarFechaReserva = (nuevaFecha, nuevaHora) => {
-    const unido = nuevaFecha + " " + nuevaHora
-    setReserva(prevState => ({
-      ...prevState,
-      usuario: actualizadoEmail,
-      fecha: unido
-    }));
-
-    Swal.fire({
-      title: "Reserva realizada con exito!",
-      icon: "success"
-    })
-    console.log("SOY YO", reserva)
-    crearReserva(reserva)
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setReserva({
+      ...reserva,
+      [name]: value
+    });
   };
 
-useEffect(()=>
-{
-  const actualizadoEmail = emailLogueado
-  setReserva(prevState => ({
-    ...prevState,
-    usuario: actualizadoEmail
-  }));
-},[reserva.comensales])
-
-  const backComensal = () => { //Funciom volver para el boton
-    if (showComensales) {
-      setReserva(prevReserva => ({  // ...prevReserva conserva el estado de mi componente reserva con sus valores anteriores
-        ...prevReserva,
-        sucursal: ""
-      }));
-      setShowComensales(false)
-      setShowSucursales(true)
-    }
-    if (showServicio) {
-      setReserva(prevReserva => ({  // ...prevReserva conserva el estado de mi componente reserva con sus valores anteriores
-        ...prevReserva,
-        comensales: 0
-      }));
-      setShowServicio(false)
-      setShowComensales(true)
-    }
-
-    if (showFecha) {
-      setReserva(prevReserva => ({  // ...prevReserva conserva el estado de mi componente reserva con sus valores anteriores
-        ...prevReserva,
-        servicio: ""
-      }));
-
-      setShowFecha(false)
-      setShowServicio(true)
-
-    }
-
-  }
-  const nextComensalesFunction = () => {
-    setReserva(prevReserva => ({  // ...prevReserva conserva el estado de mi componente reserva con sus valores anteriores
-      ...prevReserva,
-      comensales: cantidad
-    }));
-    setShowComensales(false)
-    setShowServicio(true)
-  }
-  const nextServicioFunction = (servicioElejido) => {
-    setReserva(prevReserva => ({  // ...prevReserva conserva el estado de mi componente reserva con sus valores anteriores
-      ...prevReserva,
-      servicio: servicioElejido
-    }));
-    setShowServicio(false)
-    setShowFecha(true)
-
-  }
-
-
-
-  useEffect(() => {
-    document.title = "Reservar ahora";
-  }, [])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Reserva enviada:', reserva);
+    crearReserva(reserva);
+    setReserva({
+      usuario: '',
+      sucursal: '',
+      servicio: '',
+      comensales: '',
+      fecha: ''
+    });
+  };
 
   return (
     <>
-      <Titulo link={"https://images.unsplash.com/photo-1513883049090-d0b7439799bf?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} texto={"Piano con filtros blanco y negro"} nombre={"Reservas"} />
-      {!emailLogueado ? (
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Usuario</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="por ejemplo, Juan Lopez"
+            name="usuario"
+            value={reserva.usuario}
+            onChange={handleChange}
+            required
+            minLength={3}
+            maxLength={85}
+          />
+        </Form.Group>
 
+        <Form.Group>
+          <Form.Label>Sucursal</Form.Label>
+          <Form.Select
+            aria-label="Default select example"
+            name="sucursal"
+            value={reserva.sucursal}
+            onChange={handleChange}
+          >
+            <option value="Chacabuco 474">Chacabuco 474</option>
+            <option value="San Martín 821">San Martín 821</option>
+            <option value="Gral. Paz 576">Gral. Paz 576</option>
+          </Form.Select>
+        </Form.Group>
 
+        <Form.Group>
+          <Form.Label>Servicio</Form.Label>
+          <Form.Select
+            aria-label="Default select example"
+            name="servicio"
+            value={reserva.servicio}
+            onChange={handleChange}
+          >
+            <option value="Almuerzo">Almuerzo</option>
+            <option value="Cena">Cena</option>
+          </Form.Select>
+        </Form.Group>
 
-        <>
-          <h1 style={{ textAlign: "center", marginBottom: "4rem", marginTop:"3rem"}}>Te damos la bienvenida !!</h1>
-          <div className='montserrat-font div_conteiner'>
-            <div>
-              <Button onClick={() => backComensal()} variant='dark' className=' backButton '>
-                <FontAwesomeIcon icon={faArrowLeft} />
-              </Button>
-              <div className='divButtons'>
-                {reserva.sucursal ? <Button variant='light'>Sucursal: {reserva.sucursal}</Button> : null}
-                {reserva.servicio ? <Button variant='light'>Servicio: {reserva.servicio}</Button> : null}
-                {reserva.comensales ? <Button variant='light'>Comensales: {reserva.comensales}</Button> : null}
-                {reserva.fecha ? <Button variant='light'>Fecha: {reserva.fecha}</Button> : null}
-              </div>
-            </div>
+        <Form.Group>
+          <Form.Label>Comensales</Form.Label>
+          <Form.Control
+            type="number"
+            min={2}
+            max={15}
+            name="comensales"
+            value={reserva.comensales}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
 
-            <div className='conteiner_div '>
-              {showSucursales ? (
-                <>
-                  <h2>Sucursales</h2>
-                  <Button variant='light' onClick={(e) => setReserva({ sucursal: e.target.textContent })} className='sucursalButton mb-lg-3' >Chacabuco 474 </Button>
-                  <Button variant='light' onClick={(e) => setReserva({ sucursal: e.target.textContent })} className='sucursalButton mb-lg-3'  >San Martin 821</Button>
-                  <Button variant='light' onClick={(e) => setReserva({ sucursal: e.target.textContent })} className='sucursalButton  mb-lg-3' >Gral. Paz 576</Button>
-                  {reserva.sucursal ? setShowComensales(true) & setShowSucursales(false) : null}
-                </>
-              ) : null}
+        <Form.Group id="grupoFecha">
+          <Form.Label>Fecha</Form.Label>
+          <Form.Control
+            type="datetime-local"
+            name="fecha"
+            value={reserva.fecha}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
 
-              {showComensales ? <>  {/* Si show comensales es TRUE se muestra esto*/}
-                <h2>Escoja la cantidad de comensales</h2>
-                <div className='div_comensales'>
-                  <button className='comensales_button' onClick={() => setCantidad(cantidad - 1)}><h4>-</h4></button>
-                  {cantidad < 1 ? setCantidad(1) : null}
-                  {cantidad > 15 ? setCantidad(15) : null}
-                  {console.log(cantidad)}
-                  <div className='comensales_cantidad'> <h4> {cantidad}</h4> </div>
-                  <button className='comensales_button' onClick={() => setCantidad(cantidad + 1)}><h4>+</h4></button>
-
-                </div>
-                <Button variant='light' onClick={() => nextComensalesFunction()} ><FontAwesomeIcon icon={faArrowRight} /></Button>
-              </>
-                : null}
-              {showServicio ? <><h2 className='mb-lg-3'>Elija el servicio que le gustaria recibir</h2>
-                <Button onClick={(e) => nextServicioFunction(e.target.textContent)} className='sucursalButton mb-lg-3' variant='light'>Almuerzo</Button>
-                <Button onClick={(e) => nextServicioFunction(e.target.textContent)} className='sucursalButton mb-lg-3' variant='light'>Cena</Button></> : null}
-              {showFecha ? <>
-
-                <Fecha reserva={reserva} actualizarFechaReserva={(nuevaFecha, nuevaHora) => { actualizarFechaReserva(nuevaFecha, nuevaHora) }} />
-
-              </> : null}
-            </div>
-
-          </div>
-
-        </>
-      )
-        : (
-          // Si el usuario no está logueado
-          <h1 className='text-dark'>Debe estar logueado para solicitar una reserva</h1>
-        )}<Footer />
+        <div className="divBoton">
+          <Button type="submit" className="btn-danger">
+            Guardar
+          </Button>
+        </div>
+      </Form>
     </>
   );
-}
+};
 
-export default Reserva
+export default FormularioReserva;
+
+
+
+// const Reserva = () => {
+//   const { reservas, crearReserva } = useContext(ReservasContext)
+//   const { emailLogueado } = useContext(UsuariosContext)
+
+
+
+//   const [showSucursales, setShowSucursales] = useState(true)
+//   const [showComensales, setShowComensales] = useState(false)
+//   const [showServicio, setShowServicio] = useState(false)
+//   const [showFecha, setShowFecha] = useState(false)
+//   const [cantidad, setCantidad] = useState(1)
+//   const [reserva, setReserva] = useState({
+//     _id: "",
+//     usuario: "",
+//     sucursal: "",
+//     servicio: "",
+//     comensales: 0,
+//     fecha: ""
+
+//   })
+
+
+//   const actualizarFechaReserva = (nuevaFecha, nuevaHora) => {
+//     const unido = nuevaFecha + " " + nuevaHora
+//     setReserva(prevState => ({
+//       ...prevState,
+//       usuario: actualizadoEmail,
+//       fecha: unido
+//     }));
+
+//     Swal.fire({
+//       title: "Reserva realizada con exito!",
+//       icon: "success"
+//     })
+//     console.log("SOY YO", reserva)
+//     crearReserva(reserva)
+//   };
+
+// useEffect(()=>
+// {
+//   const actualizadoEmail = emailLogueado
+//   setReserva(prevState => ({
+//     ...prevState,
+//     usuario: actualizadoEmail
+//   }));
+// },[reserva.comensales])
+
+//   const backComensal = () => { //Funciom volver para el boton
+//     if (showComensales) {
+//       setReserva(prevReserva => ({  // ...prevReserva conserva el estado de mi componente reserva con sus valores anteriores
+//         ...prevReserva,
+//         sucursal: ""
+//       }));
+//       setShowComensales(false)
+//       setShowSucursales(true)
+//     }
+//     if (showServicio) {
+//       setReserva(prevReserva => ({  // ...prevReserva conserva el estado de mi componente reserva con sus valores anteriores
+//         ...prevReserva,
+//         comensales: 0
+//       }));
+//       setShowServicio(false)
+//       setShowComensales(true)
+//     }
+
+//     if (showFecha) {
+//       setReserva(prevReserva => ({  // ...prevReserva conserva el estado de mi componente reserva con sus valores anteriores
+//         ...prevReserva,
+//         servicio: ""
+//       }));
+
+//       setShowFecha(false)
+//       setShowServicio(true)
+
+//     }
+
+//   }
+//   const nextComensalesFunction = () => {
+//     setReserva(prevReserva => ({  // ...prevReserva conserva el estado de mi componente reserva con sus valores anteriores
+//       ...prevReserva,
+//       comensales: cantidad
+//     }));
+//     setShowComensales(false)
+//     setShowServicio(true)
+//   }
+//   const nextServicioFunction = (servicioElejido) => {
+//     setReserva(prevReserva => ({  // ...prevReserva conserva el estado de mi componente reserva con sus valores anteriores
+//       ...prevReserva,
+//       servicio: servicioElejido
+//     }));
+//     setShowServicio(false)
+//     setShowFecha(true)
+
+//   }
+
+
+
+//   useEffect(() => {
+//     document.title = "Reservar ahora";
+//   }, [])
+
+//   return (
+//     <>
+//       <Titulo link={"https://images.unsplash.com/photo-1513883049090-d0b7439799bf?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} texto={"Piano con filtros blanco y negro"} nombre={"Reservas"} />
+//       {!emailLogueado ? (
+
+
+
+//         <>
+//           <h1 style={{ textAlign: "center", marginBottom: "4rem", marginTop:"3rem"}}>Te damos la bienvenida !!</h1>
+//           <div className='montserrat-font div_conteiner'>
+//             <div>
+//               <Button onClick={() => backComensal()} variant='dark' className=' backButton '>
+//                 <FontAwesomeIcon icon={faArrowLeft} />
+//               </Button>
+//               <div className='divButtons'>
+//                 {reserva.sucursal ? <Button variant='light'>Sucursal: {reserva.sucursal}</Button> : null}
+//                 {reserva.servicio ? <Button variant='light'>Servicio: {reserva.servicio}</Button> : null}
+//                 {reserva.comensales ? <Button variant='light'>Comensales: {reserva.comensales}</Button> : null}
+//                 {reserva.fecha ? <Button variant='light'>Fecha: {reserva.fecha}</Button> : null}
+//               </div>
+//             </div>
+
+//             <div className='conteiner_div '>
+//               {showSucursales ? (
+//                 <>
+//                   <h2>Sucursales</h2>
+//                   <Button variant='light' onClick={(e) => setReserva({ sucursal: e.target.textContent })} className='sucursalButton mb-lg-3' >Chacabuco 474 </Button>
+//                   <Button variant='light' onClick={(e) => setReserva({ sucursal: e.target.textContent })} className='sucursalButton mb-lg-3'  >San Martin 821</Button>
+//                   <Button variant='light' onClick={(e) => setReserva({ sucursal: e.target.textContent })} className='sucursalButton  mb-lg-3' >Gral. Paz 576</Button>
+//                   {reserva.sucursal ? setShowComensales(true) & setShowSucursales(false) : null}
+//                 </>
+//               ) : null}
+
+//               {showComensales ? <>  {/* Si show comensales es TRUE se muestra esto*/}
+//                 <h2>Escoja la cantidad de comensales</h2>
+//                 <div className='div_comensales'>
+//                   <button className='comensales_button' onClick={() => setCantidad(cantidad - 1)}><h4>-</h4></button>
+//                   {cantidad < 1 ? setCantidad(1) : null}
+//                   {cantidad > 15 ? setCantidad(15) : null}
+//                   {console.log(cantidad)}
+//                   <div className='comensales_cantidad'> <h4> {cantidad}</h4> </div>
+//                   <button className='comensales_button' onClick={() => setCantidad(cantidad + 1)}><h4>+</h4></button>
+
+//                 </div>
+//                 <Button variant='light' onClick={() => nextComensalesFunction()} ><FontAwesomeIcon icon={faArrowRight} /></Button>
+//               </>
+//                 : null}
+//               {showServicio ? <><h2 className='mb-lg-3'>Elija el servicio que le gustaria recibir</h2>
+//                 <Button onClick={(e) => nextServicioFunction(e.target.textContent)} className='sucursalButton mb-lg-3' variant='light'>Almuerzo</Button>
+//                 <Button onClick={(e) => nextServicioFunction(e.target.textContent)} className='sucursalButton mb-lg-3' variant='light'>Cena</Button></> : null}
+//               {showFecha ? <>
+
+//                 <Fecha reserva={reserva} actualizarFechaReserva={(nuevaFecha, nuevaHora) => { actualizarFechaReserva(nuevaFecha, nuevaHora) }} />
+
+//               </> : null}
+//             </div>
+
+//           </div>
+
+//         </>
+//       )
+//         : (
+//           // Si el usuario no está logueado
+//           <h1 className='text-dark'>Debe estar logueado para solicitar una reserva</h1>
+//         )}<Footer />
+//     </>
+//   );
+// }
+
+// export default Reserva
